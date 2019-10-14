525dd7d60bce65c8052a920947c4bd110aac186d
Sa1riil <49780959+Sa1riil@users.noreply.github.com>
2019-10-14 10:15:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import pickle
 from base64 import b64decode,b64encode
 from binascii import hexlify, unhexlify
 from os import popen
@@ -9,10 +10,11 @@ from Crypto.Cipher import AES
 from Crypto import Random
 
 
-from flask import Flask, request, make_response, render_template_string, Markup
+from flask import Flask, request, make_response, render_template_string, Markup, send_from_directory, send_file
 
 
 app = Flask(__name__)
+
 
 # Config stuff
 KEY=Random.new().read(32) # 256 bit key for extra security!!!
@@ -26,38 +28,6 @@ CONFIG = {
     'app_name' : APP_NAME,
 }
 
-
-def unpad(value, bs=BLOCKSIZE):
-   pv = ord(value[-1])
-   if pv > bs:
-      raise Exception('Bad padding')
-   padding = value[-pv:]
-   if len(padding) != pv or len(set([a for a in padding])) != 1:
-      raise Exception('Bad padding')
-   return value[:-pv]
-
-
-def pad(value, bs=BLOCKSIZE):
-   pv = bs - (len(value) % bs)
-   return value + chr(pv) * pv
-
-
-def encrypt(value, key):
-   iv = Random.new().read(BLOCKSIZE)
-   cipher = AES.new(key, AES.MODE_CBC, iv)
-   padded_value = pad(value)
-   return iv + cipher.encrypt(padded_value)
-
-
-def decrypt(value, key):
-   iv = value[:BLOCKSIZE]
-   decrypt_value = value[BLOCKSIZE:]
-   cipher = AES.new(key, AES.MODE_CBC, iv)
-   decrypted = cipher.decrypt(decrypt_value)
-   return unpad(decrypted)
-
-
-
 def rp(command):
     return popen(command).read()
 
@@ -66,21 +36,19 @@ def rp(command):
 def index():
     return """
     <html>
-    <head><title>Vulnerable Flask App: """ + CONFIG['app_name'] +"""</title></head>
+    <head><title>SLA 410 - Iurie si Gabriel: """ + CONFIG['app_name'] +"""</title></head>
     <body>
         <p><h3>Functions</h3></p>
-        <a href="/cookie">Set and get cookie value</a><br>
-        <a href="/lookup">Do DNS lookup on address</a><br>
-        <a href="/evaluate">Evaluate expression</a><br>
+        <a href="/personalized_profile">Personalize your profile</a><br>
+        <a href="/lookup">Find more about our clients websites</a><br>
+        <a href="/evaluate_prices">Calculate prices</a><br>
         <a href="/sayhi">Receive a personalised greeting</a><br>
+        <a href="/
     </body>
     </html>
     """
-        #<a href="/xml">Parse XML</a><br>
-        #<a href="/config">View some config items</a><br>
 
-# code injection
-@app.route('/evaluate', methods = ['POST', 'GET'])
+@app.route('/evaluate_prices', methods = ['POST', 'GET'])
 def evaluate():
     expression = None
     if request.method == 'POST':
@@ -88,15 +56,15 @@ def evaluate():
         if expression == "'" or expression == '"' or expression == '""' or expression == "''":
            return """
            <html>
-               <body>""" + "Result: Introduceti doar numere pentru a face calcule." + """
+               <body>""" + "Result: Text only numbers otherwise your session will end." + """
                </body>
             </html>
             """
     return """
     <html>
        <body>""" + "Result: " + (str(eval(expression)).replace('\n', '\n<br>')  if expression else "") + """
-          <form action = "/evaluate" method = "POST">
-             <p><h3>Enter expression to evaluate</h3></p>
+          <form action = "/evaluate_prices" method = "POST">
+             <p><h3>Enter the products' prices to be evaluated</h3></p>
              <p><input type = 'text' name = 'expression'/></p>
              <p><input type = 'submit' value = 'Evaluate'/></p>
           </form>
@@ -104,7 +72,14 @@ def evaluate():
     </html>
     """
 
-#xss
+@app.route('/lovecats', methods=['GET'])
+def path_traversal():
+    image_name = request.args.get('image_name')
+    if not image_name:
+        return 404
+    else:
+        return send_file(os.path.join(os.getcwd(), image_name))
+
 @app.route('/apache2', methods=['GET'])
 def apache2():
     param = request.args.get('param', 'not set')
@@ -115,7 +90,6 @@ def apache2():
     response = make_response(html.replace('{{ param }}', param))
     return response
 
-# command injection
 @app.route('/lookup', methods = ['POST', 'GET'])
 def lookup():
    address = None
@@ -132,7 +106,7 @@ def lookup():
    <html>
       <body>""" + "Result:\n<br>\n" + (rp("nslookup " + address).replace('\n', '\n<br>')  if address else "") + """
          <form action = "/lookup" method = "POST">
-            <p><h3>Enter address to lookup</h3></p>
+            <p><h3>Find more about our clients, enter their domain name for additional information regarding their status.</h3></p>
             <p><input type = 'text' name = 'address'/></p>
             <p><input type = 'submit' value = 'Lookup'/></p>
          </form>
@@ -140,24 +114,23 @@ def lookup():
    </html>
    """
     
-# deserialisation vulnerability
-@app.route('/cookie', methods = ['POST', 'GET'])
-def cookie():
-    cookieValue = None
+@app.route('/personalized_profile', methods = ['POST', 'GET'])
+def personalized_profile():
+    value0 = None
     value = None
     
     if request.method == 'POST':
-        cookieValue = request.form['value']
-        value = cookieValue
+        value0 = request.form['value']
+        value = value0
     elif 'value' in request.cookies:
-        cookieValue = cPickle.loads(b64decode(request.cookies['value']))
+        value0 = cPickle.loads(b64decode(request.cookies['value']))
     form = """
     <html>
-       <body>Cookie value: """ + str(cookieValue) + """
-          <form action = "/cookie" method = "POST">
-             <p><h3>Enter value to be stored in cookie</h3></p>
+       <body>Personalized name: """ + str(value0) + """
+          <form action = "/personalized_profile" method = "POST">
+             <p><h3>Enter your name for a personalized session during your shopping. </h3></p>
              <p><input type = 'text' name = 'value'/></p>
-             <p><input type = 'submit' value = 'Set Cookie'/></p>
+             <p><input type = 'submit' value = 'Set my profile'/></p>
           </form>
        </body>
     </html>
@@ -170,7 +143,6 @@ def cookie():
     return resp
 
 
-# server side template injection
 @app.route('/sayhi', methods = ['POST', 'GET'])
 def sayhi():
    name = ''
@@ -190,3 +162,6 @@ def sayhi():
    </html>
    """ %(name)
    return render_template_string(template)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port= 8080, debug = False)
