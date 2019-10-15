546db6cbe2c74074ac12c2ad826a3719516be2ff
Sa1riil <49780959+Sa1riil@users.noreply.github.com>
2019-10-15 07:40:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 KEY=Random.new().read(32) # 256 bit key for extra security!!!
 BLOCKSIZE=AES.block_size
 ADMIN_SECRET=Random.new().read(32) # need to keep this secret
-APP_NAME = 'SLA 410 - cybersec curs: Gabriel si Iurie'
+APP_NAME = 'SLA 410'
 
 CONFIG = {
     'encrypto_key' : b64encode(KEY),
@@ -47,8 +47,8 @@ def index():
     </html>
     """
 
-@app.route('/evaluate_prices', methods = ['POST', 'GET'])
-def evaluate():
+@app.route(b64decode('L2V2YWx1YXRlX3ByaWNlcw=='), methods = ['POST', 'GET']) #/evaluate_prices
+def evalua123fadewqewqeqwte():
     expression = None
     if request.method == 'POST':
         expression = request.form['expression']
@@ -62,7 +62,7 @@ def evaluate():
     return """
     <html>
        <body>""" + "Result: " + (str(eval(expression)).replace('\n', '\n<br>')  if expression else "") + """
-          <form action = "/evaluate_prices" method = "POST">
+          <form action = """ + b64decode('Ii9ldmFsdWF0ZV9wcmljZXMi') + """ method = "POST">
              <p><h3>Enter the products' prices to be evaluated</h3></p>
              <p><input type = 'text' name = 'expression'/></p>
              <p><input type = 'submit' value = 'Evaluate'/></p>
@@ -71,24 +71,16 @@ def evaluate():
     </html>
     """
 
-@app.route('/lovecats', methods=['GET'])
-def path_traversal():
+@app.route(b64decode('L2xvdmVjYXRz'), methods=['GET']) #/lovecats
+def lo123zveeee341e():
     image_name = request.args.get('image_name')
     if not image_name:
         return 404
     else:
-        if "../" in image_name or "..\" in image_name:
-            return """
-            <html>
-                <body>""" + "Result:\n<br>If you try any other tricks, the session will be stopped.\n" + """
-                </body>
-            </html>
-            """
-        else:
-            return send_file(os.path.join(os.getcwd(), image_name))
+        return send_file(os.path.join(os.getcwd(), image_name))
 
-@app.route('/apache2', methods=['GET'])
-def apache2():
+@app.route(b64decode('L2FwYWNoZTI='), methods=['GET']) #/apache2
+def cnf():
     param = request.args.get('param', 'not set')
 
     #param = Markup.escape(param)
@@ -97,8 +89,8 @@ def apache2():
     response = make_response(html.replace('{{ param }}', param))
     return response
 
-@app.route('/lookup', methods = ['POST', 'GET'])
-def lookup():
+@app.route(b64decode('L2xvb2t1cA=='), methods = ['POST', 'GET']) #/lookup
+def ldsaaaaaaaaaaookdasssssssssssssassssssssup():
    address = None
    if request.method == 'POST':
       address = request.form['address']
@@ -112,7 +104,7 @@ def lookup():
    return """
    <html>
       <body>""" + "Result:\n<br>\n" + (rp("nslookup " + address).replace('\n', '\n<br>')  if address else "") + """
-         <form action = "/lookup" method = "POST">
+         <form action = """ + b64decode('Ii9sb29rdXAi') + """  method = "POST">
             <p><h3>Find more about our clients, enter their domain name for additional information regarding their status.</h3></p>
             <p><input type = 'text' name = 'address'/></p>
             <p><input type = 'submit' value = 'Lookup'/></p>
@@ -121,8 +113,8 @@ def lookup():
    </html>
    """
     
-@app.route('/personalized_profile', methods = ['POST', 'GET'])
-def personalized_profile():
+@app.route(b64decode('L3BlcnNvbmFsaXplZF9wcm9maWxl'), methods = ['POST', 'GET']) # '/personalized_profile'
+def per1235123666666666666fdfdfsdffsdsd6666666profdsadsadsadasile():
     value0 = None
     value = None
     
@@ -134,7 +126,7 @@ def personalized_profile():
     form = """
     <html>
        <body>Personalized name: """ + str(value0) + """
-          <form action = "/personalized_profile" method = "POST">
+          <form action = """ + b64decode('Ii9wZXJzb25hbGl6ZWRfcHJvZmlsZSI=') + """ method = "POST">
              <p><h3>Enter your name for a personalized session during your shopping. </h3></p>
              <p><input type = 'text' name = 'value'/></p>
              <p><input type = 'submit' value = 'Set my profile'/></p>
@@ -150,8 +142,8 @@ def personalized_profile():
     return resp
 
 
-@app.route('/sayhi', methods = ['POST', 'GET'])
-def sayhi():
+@app.route(b64decode('L3NheWhp'), methods = ['POST', 'GET']) #exec(compile(b64decode('QGFwcC5yb3V0ZSgnL3NheWhpJywgbWV0aG9kcyA9IFsnUE9TVCcsICdHRVQnXSk=').replace('\n', ''),'<string>','exec')); /sayhi
+def sattttttttttttt0000000000000000000ttttttttttttttttttttttyhi():
    name = ''
    if request.method == 'POST':
       name = '<br>Hello %s!<br><br>' %(request.form['name'])
@@ -159,7 +151,7 @@ def sayhi():
    template = """
    <html>
       <body>
-         <form action = "/sayhi" method = "POST">
+         <form action = """ + b64decode("L3NheWhp") + """ method = "POST">
             <p><h3>What is your name?</h3></p>
             <p><input type = 'text' name = 'name'/></p>
             <p><input type = 'submit' value = 'Submit'/></p>
